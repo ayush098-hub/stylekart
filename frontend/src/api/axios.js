ccef9011782fbb104a358e461d4d9960b781af9c
Ayush <ayush@aml.com>
2026-04-05 02:00:55 +0530

    added alb url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const API = axios.create({
     baseURL: 'http://k8s-default-stylekar-4aa298fc3a-60568287.ap-south-1.elb.amazonaws.com',
-,
 });
 
 API.interceptors.request.use((config) => {
